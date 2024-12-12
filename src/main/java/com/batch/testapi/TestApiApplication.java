aad7df20b8403ace75c0f8986b9fca9cb21730f6
heojungyoung <tyireal@gmail.com>
2024-12-12 21:46:28 +0900

    init

---
+++ src/main/java/com/batch/testapi/TestApiApplication.java
@@ -0,0 +1,14 @@
+package com.batch.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApiApplication.class, args);
+    }
+
+}
