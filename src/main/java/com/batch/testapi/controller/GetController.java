c758c7c7f87c3d19398291d641a7df6306d64810
heojungyoung <tyireal@gmail.com>
2024-12-13 21:14:01 +0900

    standalone redis

--- src/main/java/com/batch/testapi/controller/GetController.java
+++ src/main/java/com/batch/testapi/controller/GetController.java
@@ -2,20 +2,24 @@ package com.batch.testapi.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/main/v1")
 public class GetController {
 
+
+   // http://localhost:8080/main/v1/check
+   // {{testApi}}/main/v1/check
+
     private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
 
-    // http://localhost:8080/main/v1/check
-    // {{testApi}}/main/v1/check
+
     @RequestMapping(value = "/check", method = RequestMethod.GET)
-    public String getHello() {
+    @Cacheable(key = "#version", value = "svcsupport")
+    public String getHello(@RequestParam(name = "version", required = false) String version){
+        LOGGER.info(version);
         LOGGER.info("getHello 메소드가 호출되었습니다.");
         return "check version";
     }
---
+++ src/main/java/com/batch/testapi/redis/RedisConfig.java
@@ -0,0 +1,60 @@
+package com.batch.testapi.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import java.time.Duration;
+import java.util.Objects;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    RedisCacheManager cacheManager(RedisTemplate<String, Object> template) {
+        RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(Objects.requireNonNull(template.getConnectionFactory()))
+                .cacheDefaults(getCacheConfigurationWithTtl(template)) // 모든 캐시에 대해 기본 설정 적용
+                .build();
+        redisCacheManager.setTransactionAware(true);
+        return redisCacheManager;
+    }
+
+
+    private RedisCacheConfiguration getCacheConfigurationWithTtl(RedisTemplate<String, Object> template) {
+        return RedisCacheConfiguration
+                .defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getStringSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getValueSerializer()))
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(160));
+    }
+
+}
