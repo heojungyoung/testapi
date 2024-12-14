e4c43c07468461e122bb5c2fcdeadfe660eb37ad
heojungyoung <tyireal@gmail.com>
2024-12-14 19:40:49 +0900

    cluster redis

--- src/main/java/com/batch/testapi/redis/RedisConfig.java
+++ src/main/java/com/batch/testapi/redis/RedisConfig.java
@@ -1,11 +1,16 @@
 package com.batch.testapi.redis;
 
+import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -15,7 +20,7 @@ import java.util.Objects;
 
 
 @Configuration
-public class RedisConfig {
+public class RedisConfig implements CachingConfigurer {
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -24,8 +29,33 @@ public class RedisConfig {
     private int port;
 
     @Bean
-    RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    LettuceConnectionFactory lettuceConnectionFactory() {
+        ClusterTopologyRefreshOptions topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enablePeriodicRefresh()
+                .enableAllAdaptiveRefreshTriggers()
+                .build();
+
+        ClusterClientOptions clusterClientOptions =	ClusterClientOptions.builder()
+                .topologyRefreshOptions(topologyRefreshOptions)
+                .validateClusterNodeMembership(false).build();
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(clusterClientOptions)
+                .readFrom(ReadFrom.ANY)
+                .commandTimeout(Duration.ofSeconds(160)).build();
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        redisClusterConfiguration.clusterNode(host,port);
+        redisClusterConfiguration.clusterNode("127.0.0.1",7002);
+        redisClusterConfiguration.clusterNode("127.0.0.1",7003);
+        redisClusterConfiguration.clusterNode("127.0.0.1",7004);
+        redisClusterConfiguration.clusterNode("127.0.0.1",7005);
+        redisClusterConfiguration.clusterNode("127.0.0.1",7006);
+
+        final LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisClusterConfiguration,
+                clientConfig);
+        lettuceConnectionFactory.setValidateConnection(true);
+        return new LettuceConnectionFactory(redisClusterConfiguration, clientConfig);
     }
 
     @Bean
@@ -33,7 +63,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         return redisTemplate;
     }
 
