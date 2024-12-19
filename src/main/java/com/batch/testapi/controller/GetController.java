cd38f56807440448aab23e1ea2569c0fb36a61af
heojungyoung <tyireal@gmail.com>
2024-12-19 22:52:50 +0900

    data init

--- src/main/java/com/batch/testapi/controller/GetController.java
+++ src/main/java/com/batch/testapi/controller/GetController.java
@@ -2,8 +2,11 @@ package com.batch.testapi.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController
 @RequestMapping("/main/v1")
@@ -17,10 +20,33 @@ public class GetController {
 
 
     @RequestMapping(value = "/check", method = RequestMethod.GET)
-    @Cacheable(key = "#version", value = "svcsupport")
     public String getHello(@RequestParam(name = "version", required = false) String version){
         LOGGER.info(version);
         LOGGER.info("getHello 메소드가 호출되었습니다.");
         return "check version";
     }
+
+
+    @GetMapping("/greet")
+    public Map<String, String> greet() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, World!");
+        response.put("status", "success");
+        return response; // Automatically serialized to JSON
+    }
+
+
+    @GetMapping("/treemap")
+    public TreeMap<Integer, String> treemapTest() {
+
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+        for(int i = 20 ; i >0; i-=2 ){
+            treeMap.put(i, i + "value");
+        }
+        // LOGGER.info(treeMap.toString());
+
+        return treeMap; // Automatically serialized to JSON
+    }
+
 }
