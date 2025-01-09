33debe6075d01dbe7ea1b7c564d4137e1ee51e74
tyireal <tyireal@morusoft.com>
2025-01-09 14:41:10 +0900

    Profile 적용

--- src/main/java/com/batch/testapi/controller/GetController.java
+++ src/main/java/com/batch/testapi/controller/GetController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/main/v1")
 public class GetController {
 
+   // -Dspring.profiles.active=test
 
    // http://localhost:8080/main/v1/check
    // {{testApi}}/main/v1/check
 
-    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+    private final Logger logger = LoggerFactory.getLogger(GetController.class);
 
-
-    @RequestMapping(value = "/check", method = RequestMethod.GET)
+    @GetMapping(value = "/check")
     @Cacheable(key = "#version", value = "svcsupport")
     public String getHello(@RequestParam(name = "version", required = false) String version){
-        LOGGER.info(version);
-        LOGGER.info("getHello 메소드가 호출되었습니다.");
+        logger.info(version);
+        logger.info("getHello 메소드가 호출되었습니다.");
         return "check version";
     }
 }
